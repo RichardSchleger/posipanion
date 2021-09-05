1efd726023871e815fab30e8f0b21743cd1669ad
Richard Schleger <schleger13@gmail.com>
2021-09-05 09:58:31 +0200

    Add jwt token authentication

--- src/main/java/sk/richardschleger/posipanion/repositories/UserRepository.java
+++ src/main/java/sk/richardschleger/posipanion/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package sk.richardschleger.posipanion.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import sk.richardschleger.posipanion.entities.User;
 
 public interface UserRepository extends CassandraRepository<User, UUID>{
     
+    @AllowFiltering
+    Optional<User> findByEmail(String email);
+
 }
---
+++ src/main/java/sk/richardschleger/posipanion/services/JwtUserDetailsService.java
@@ -0,0 +1,29 @@
+package sk.richardschleger.posipanion.services;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import sk.richardschleger.posipanion.entities.User;
+import sk.richardschleger.posipanion.repositories.UserRepository;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("No user with given email found!"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+	}
+}
