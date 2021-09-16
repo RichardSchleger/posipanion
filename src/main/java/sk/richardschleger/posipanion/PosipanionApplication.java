25a7896b575e45d5f5f53ad58929f9654a11b103
Richard Schleger <schleger13@gmail.com>
2021-09-16 07:54:33 +0200

    Add firebase setup

--- src/main/java/sk/richardschleger/posipanion/PosipanionApplication.java
+++ src/main/java/sk/richardschleger/posipanion/PosipanionApplication.java
@@ -1,10 +1,31 @@
 package sk.richardschleger.posipanion;
 
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class PosipanionApplication {
+
+	@Bean
+	FirebaseMessaging firebaseMessaging() throws IOException {
+		GoogleCredentials googleCredentials = GoogleCredentials
+				.fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+		FirebaseOptions firebaseOptions = FirebaseOptions
+				.builder()
+				.setCredentials(googleCredentials)
+				.build();
+		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+		return FirebaseMessaging.getInstance(app);
+	}
 
 	public static void main(String[] args) {
 		System.setProperty("datastax-java-driver.basic.request.timeout", "10 seconds");
