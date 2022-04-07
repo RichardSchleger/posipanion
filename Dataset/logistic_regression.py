34981c8a0074fb1834713afa105dcbeb6afb9196
Richard Schleger <schleger13@gmail.com>
2022-04-07 20:43:55 +0200

    visualize logistic regression for fall detection

--- Dataset/logistic_regression.py
+++ Dataset/logistic_regression.py
@@ -4,6 +4,23 @@ from sklearn.linear_model import LogisticRegression
 
 import matplotlib.pyplot as plt
 
+def visualize(X, y, model):
+    # Create a mesh to plot in
+    h = 0.01
+    x_min, x_max = X[0].min() - 1, X[0].max() + 1
+    y_min, y_max = X[1].min() - 1, X[1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # Make predictions on this mesh
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[0], X[1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
 dataset = pd.read_csv('dataset.csv', sep=',', header=None, skiprows=1, encoding='ISO-8859-1')
 
 x = dataset.loc[:, [0, 1]]
@@ -22,3 +39,6 @@ w1, w2 = clf.coef_.T
 print("w1 ", w1[0])
 print("w2 ", w2[0])
 print("b ", b)
+
+visualize(x, y, clf)
+
