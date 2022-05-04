68ba1ee1055f883a417060a728a247bd938b745a
Richard Schleger <schleger13@gmail.com>
2022-05-04 08:59:31 +0200

    Small changes to Firebase BE

--- backend/src/main/java/sk/richardschleger/posipanion/PosipanionApplication.java
+++ backend/src/main/java/sk/richardschleger/posipanion/PosipanionApplication.java
@@ -23,7 +23,7 @@ public class PosipanionApplication {
 				.builder()
 				.setCredentials(googleCredentials)
 				.build();
-		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions);
 		return FirebaseMessaging.getInstance(app);
 	}
 
